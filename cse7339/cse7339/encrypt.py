0fe560936e204c1b97e178501e0295a178cc79f4
aacarroll <aacarroll@smu.edu>
2016-12-02 23:00:43 -0600

    encrypt.py that uses master password for everything

--- cse7339/cse7339/encrypt.py
+++ cse7339/cse7339/encrypt.py
@@ -5,46 +5,64 @@ BS = AES.block_size #16
 
 def encryptFile(master, p_filename):
     #randomly generate key
-    random = Random.new().read(BS)
+    #random = Random.new().read(BS)
 
     #read plaintext file
+    key = pad(master)
     p_text = readFile(p_filename)
-    
-    #perform AES encryption on data using random key
-    e_text = encryptAES(random, p_text)
 
+    #perform AES encryption on data using random key
+    if ".txt" in p_filename:
+        e_text = encryptAES(key, p_text)
+    if ".jpg" in p_filename:
+        e_text = encryptAES_jpg(key, p_text)
     #write encrypted data to file
+
     writeFile(p_filename, e_text)
 
     #perform AES encryption on random key using master key
-    e_key = encryptAES(master, random)
+    #e_key = encryptAES(master, random)
 
-    return e_key
+    #return e_key
 
-def decryptFile(e_filename, e_key, master):
+def decryptFile(e_filename,  master):
     #perform AES decryption on random key using master key
-    random = decryptAES(master, e_key)
+    #random = decryptAES(master, e_key)
 
     #read encrypted file
     e_text = readFile(e_filename)
 
     #perform AES decryption on encrypted file using random key
-    p_text = decryptAES(random, e_text)
+    if ".txt" in e_filename:
+        p_text = decryptAES(master, e_text)
+    if ".jpg" in e_filename:
+        p_text = decryptAES_jpg(master, e_text)
 
     #write decrypted data to file
     writeFile(e_filename, p_text)
-    
+
 #perform AES encryption
 def encryptAES(key, plaintext):
     iv = Random.new().read(BS)
-    cipher = AES.new(pad(key), AES.MODE_CBC, iv)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     encrypted = iv + cipher.encrypt(pad(plaintext))
     return encrypted
 
+def encryptAES_jpg(key, plaintext):
+    iv = Random.new().read(BS)
+    cipher = AES.new(pad(key), AES.MODE_CFB, iv)
+    encrypted = iv + cipher.encrypt(pad(plaintext))
+    return encrypted
 #perform AES decryption
 def decryptAES(key, encrypted):
     iv = encrypted[:16]
     cipher = AES.new(pad(key), AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(encrypted[16:]))
+    return plaintext
+
+def decryptAES_jpg(key, encrypted):
+    iv = encrypted[:16]
+    cipher = AES.new(pad(key), AES.MODE_CFB, iv)
     plaintext = unpad(cipher.decrypt(encrypted[16:]))
     return plaintext
 
@@ -57,7 +75,7 @@ def unpad(s):
 def readFile(filename):
     myfile = open(filename, 'r+')
     mytext = myfile.read()
-    myfile.close()    
+    myfile.close()
     return mytext
 
 def writeFile(filename, data):
@@ -70,7 +88,7 @@ if __name__ == '__main__':
     password = "0000"
     txt_file = "include/test.txt"
     jpg_file = "include/test.jpg"
-    txt_key = encryptFile(password, txt_file)
-    decryptFile(txt_file, txt_key, password)
+    encryptFile(password, txt_file)
+    decryptFile(txt_file, password)
     jpg_key = encryptFile(password, jpg_file)
-    decryptFile(jpg_file, txt_key, password)
+    decryptFile(jpg_file, password)
