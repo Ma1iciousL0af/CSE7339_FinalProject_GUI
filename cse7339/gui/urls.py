874fe8d08c923e19c50bd04340f41b8c1e6f5797
Nariana <nsands@smu.edu>
2016-12-04 15:21:59 -0600

    finally got project set up correctly; using python 2.7 and django 1.10

--- cse7339/gui/urls.py
+++ cse7339/gui/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'gui'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+##    url(r'^$', views.upload_file, name='upload'),
+##    url(r'^$', views.handle_uploaded_file, name='upload_handle')
 ]
