246b4569bd64485fe77ca3f55fe70df9940f13b1
aacarroll <aacarroll@smu.edu>
2016-12-04 12:06:57 -0600

    encrypting and decrypting keys works

--- cse7339/cse7339/encrypt.py
+++ cse7339/cse7339/encrypt.py
@@ -2,6 +2,7 @@ from Crypto import Random
 from Crypto.Random import random
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import Blowfish
 import os
 from shutil import copyfile
 
@@ -39,8 +40,27 @@ def encryptFile(file_name, key):
     os.remove(file_name) # remove old plaintext file
     if ".enc" in file_name + ".enc":
         os.rename(file_name + ".enc", file_name) #rename .enc file to the original filename
-        #yes I tried doing the encryption in place, it was doing weird stuff. 
+        #yes I tried doing the encryption in place, it was doing weird stuff.
 
+def encryptKey(key_file, key):
+    with open(key_file, 'rb') as file:
+        plaintext = file.read()
+    enc = encrypt_BlowFish(plaintext, key)
+    with open(key_file + ".enc", 'wb') as file_out:
+        file_out.write(enc)
+    os.remove(key_file) # remove old plaintext file
+    if ".enc" in key_file + ".enc":
+        os.rename(key_file + ".enc", key_file) #rename .enc file to the original filename
+        #yes I tried doing the encryption in place, it was doing weird stuff.
+
+def encrypt_BlowFish(plaintext, key):
+    cipher = Blowfish.new(key)
+    return cipher.encrypt(plaintext)
+
+def decrypt_Blowfish(ciphertext, key):
+    cipher = Blowfish.new(key)
+    key = cipher.decrypt(ciphertext)
+    return key
 
 def decryptFile(file_name, key):
     with open(file_name, 'rb') as file:
@@ -50,12 +70,42 @@ def decryptFile(file_name, key):
     with open(file_name,'wb') as file:
         file.write(dec)
 
+def decryptKey(key_name, key):
+    with open(key_name, 'rb') as file:
+        ciphertext = file.read()
+        print "success"
+    dec = decrypt_Blowfish(ciphertext, key)
+    return dec
+
+def saveKey(txt_file, key):
+    f = open(txt_file + '.key', 'w')
+    f.write(key)
+    f.close()
+
+# def readKey(key_file):
+#     f = open(key_file, 'r')
+#     key = f.read()
+#     return key
+
+
 if __name__ == '__main__':
-    password = '0000'
+    password = "0000"
     key = generateKey(password)
     txt_file = "include/test.txt"
     jpg_file = "include/test.jpg"
+    #saveKey(txt_file, key)
     encryptFile(txt_file, key)
+    saveKey(txt_file, key)
+    encryptKey(txt_file + ".key",password)
+
+    if txt_file + ".key":
+        print "reading key from file"
+        key = decryptKey(txt_file + ".key", password)
     decryptFile(txt_file, key)
     encryptFile(jpg_file, key)
+    saveKey(jpg_file, key)
+    encryptKey(jpg_file + ".key", password)
+    if jpg_file + ".key":
+        print "reading key from file"
+        key = decryptKey(txt_file + ".key", password)
     decryptFile(jpg_file, key)
