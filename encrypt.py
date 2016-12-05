a4937046a4777e7662fbf7cf6f6f3a0ee40aa285
aacarroll <aacarroll@smu.edu>
2016-12-05 06:48:13 -0600

    first attempt at integrating all the things

--- encrypt.py
+++ encrypt.py
@@ -6,102 +6,118 @@ from Crypto.Cipher import Blowfish
 import os
 from shutil import copyfile
 
-def pad(s):
-    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+class Encrypt():
+    def pad(s):
+        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
 
-def generateKey(password):
-    #generate randomly generated key from master password
-    salt = Random.new().read(8)
-    iterations = 5000
-    dkLen = 32
-    key = PBKDF2(password, salt, dkLen = 32, count = iterations)
-    return key
+    def generateKey(password):
+        #generate randomly generated key from master password
+        salt = Random.new().read(8)
+        iterations = 5000
+        dkLen = 32
+        key = PBKDF2(password, salt, dkLen = 32, count = iterations)
+        return key
 
-def encrypt (message, key, key_size=256):
-    message = pad(message)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return iv + cipher.encrypt(message)
+    def encrypt (message, key, key_size=256):
+        message = pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
 
-def decrypt(ciphertext, key):
-    iv = ciphertext[:AES.block_size]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-    return plaintext.rstrip(b"\0")
+    def decrypt(ciphertext, key):
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+        return plaintext.rstrip(b"\0")
 
-def encryptFile(file_name, key):
-    with open(file_name, 'rb') as file:
-        plaintext = file.read()
-    enc = encrypt(plaintext, key)
-    with open(file_name + ".enc", 'wb') as file_out:
-        file_out.write(enc)
-    # copyfile(file_name+".enc", "encrypted")
-    # copying file, intended for testing whether encrypt/decrypt was actually working
-    os.remove(file_name) # remove old plaintext file
-    if ".enc" in file_name + ".enc":
-        os.rename(file_name + ".enc", file_name) #rename .enc file to the original filename
-        #yes I tried doing the encryption in place, it was doing weird stuff.
+    def encryptFile(file_name, key):
+        with open(file_name, 'rb') as file:
+            plaintext = file.read()
+        enc = encrypt(plaintext, key)
+        with open(file_name + ".enc", 'wb') as file_out:
+            file_out.write(enc)
+        # copyfile(file_name+".enc", "encrypted")
+        # copying file, intended for testing whether encrypt/decrypt was actually working
+        os.remove(file_name) # remove old plaintext file
+        if ".enc" in file_name + ".enc":
+            os.rename(file_name + ".enc", file_name) #rename .enc file to the original filename
+            #yes I tried doing the encryption in place, it was doing weird stuff.
 
-def encryptKey(key_file, key):
-    with open(key_file, 'rb') as file:
-        plaintext = file.read()
-    enc = encrypt_BlowFish(plaintext, key)
-    with open(key_file + ".enc", 'wb') as file_out:
-        file_out.write(enc)
-    os.remove(key_file) # remove old plaintext file
-    if ".enc" in key_file + ".enc":
-        os.rename(key_file + ".enc", key_file) #rename .enc file to the original filename
-        #yes I tried doing the encryption in place, it was doing weird stuff.
+    def encryptKey(key_file, key):
+        with open(key_file, 'rb') as file:
+            plaintext = file.read()
+        enc = encrypt_BlowFish(plaintext, key)
+        with open(key_file + ".enc", 'wb') as file_out:
+            file_out.write(enc)
+        os.remove(key_file) # remove old plaintext file
+        if ".enc" in key_file + ".enc":
+            os.rename(key_file + ".enc", key_file) #rename .enc file to the original filename
+            #yes I tried doing the encryption in place, it was doing weird stuff.
 
-def encrypt_BlowFish(plaintext, key):
-    cipher = Blowfish.new(key)
-    return cipher.encrypt(plaintext)
+    def encrypt_BlowFish(plaintext, key):
+        cipher = Blowfish.new(key)
+        return cipher.encrypt(plaintext)
 
-def decrypt_Blowfish(ciphertext, key):
-    cipher = Blowfish.new(key)
-    key = cipher.decrypt(ciphertext)
-    return key
+    def decrypt_Blowfish(ciphertext, key):
+        cipher = Blowfish.new(key)
+        key = cipher.decrypt(ciphertext)
+        return key
 
-def decryptFile(file_name, key):
-    with open(file_name, 'rb') as file:
-        ciphertext = file.read()
-        print "success"
-    dec = decrypt(ciphertext, key)
-    with open(file_name,'wb') as file:
-        file.write(dec)
+    def decryptFile(file_name, key):
+        with open(file_name, 'rb') as file:
+            ciphertext = file.read()
+            print "success"
+        dec = decrypt(ciphertext, key)
+        with open(file_name,'wb') as file:
+            file.write(dec)
 
-def decryptKey(key_name, key):
-    with open(key_name, 'rb') as file:
-        ciphertext = file.read()
-        print "success"
-    dec = decrypt_Blowfish(ciphertext, key)
-    return dec
+    def decryptKey(key_name, key):
+        with open(key_name, 'rb') as file:
+            ciphertext = file.read()
+            print "success"
+        dec = decrypt_Blowfish(ciphertext, key)
+        return dec
 
-def saveKey(txt_file, key):
-    f = open(txt_file + '.key', 'w')
-    f.write(key)
-    f.close()
+    def saveKey(txt_file, key):
+        f = open(txt_file + '.key', 'w')
+        f.write(key)
+        f.close()
 
 
+class EncryptWrapper():
+    def __init__(self):
+        self.encrypt = Encrypt()
 
-if __name__ == '__main__':
-    password = "46xwzGJub4tlc0Qd@zeDs78Y"
-    key = generateKey(password)
-    txt_file = "include/test.txt"
-    jpg_file = "include/test.jpg"
-    #saveKey(txt_file, key)
-    encryptFile(txt_file, key)
-    saveKey(txt_file, key)
-    encryptKey(txt_file + ".key",password)
+    def generateKey(password):
+        return self.encrypt.generateKey(password)
 
-    if txt_file + ".key":
-        print "reading key from file"
-        key = decryptKey(txt_file + ".key", password)
-    decryptFile(txt_file, key)
-    encryptFile(jpg_file, key)
-    saveKey(jpg_file, key)
-    encryptKey(jpg_file + ".key", password)
-    if jpg_file + ".key":
-        print "reading key from file"
-        key = decryptKey(txt_file + ".key", password)
-    decryptFile(jpg_file, key)
+    def encryptFile(file_name, key):
+        return self.encrypt.encryptFile(file_name, key)
+
+    def saveKey(key):
+        return self.encrypt.saveKey(key)
+
+    def encryptKey(key_file, password):
+        return self.encrypt.encryptKey(key_file, password) 
+
+# if __name__ == '__main__':
+#     password = "46xwzGJub4tlc0Qd@zeDs78Y"
+#     key = generateKey(password)
+#     txt_file = "include/test.txt"
+#     jpg_file = "include/test.jpg"
+#     #saveKey(txt_file, key)
+#     encryptFile(txt_file, key)
+#     saveKey(txt_file, key)
+#     encryptKey(txt_file + ".key",password)
+#
+#     if txt_file + ".key":
+#         print "reading key from file"
+#         key = decryptKey(txt_file + ".key", password)
+#     decryptFile(txt_file, key)
+#     encryptFile(jpg_file, key)
+#     saveKey(jpg_file, key)
+#     encryptKey(jpg_file + ".key", password)
+#     if jpg_file + ".key":
+#         print "reading key from file"
+#         key = decryptKey(txt_file + ".key", password)
+#     decryptFile(jpg_file, key)
--- server.py
+++ server.py
@@ -1,80 +1,107 @@
 import os
-from flask import Flask, Response, flash, request, redirect, url_for
-from flask.ext.login import LoginManager, UserMixin, login_required
+import sys
+from cloud_storage_app_database import CredentialsWrapper
+from flask import Flask, Response, flash, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
+import dropbox_functions
+import box_functions
+import google_drive_functions
+import encrypt
 
-UPLOAD_FOLDER = './uploads'
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-class User(UserMixin):
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-                     "JaneDoe": ("JaneDoe", "Jane")}
-
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
-
-    @classmethod
-    def get(cls, id):
-        return cls.user_database.get(id)
-
-@login_manager.request_loader
-def load_user(request):
-    token = request.headers.get('Authorization')
-    if token is None:
-        token = request.args.get('token')
-
-    if token is not None:
-        username, password = token.split(':')
-        user_entry = User.get(username)
-        if (user_entry is not None):
-            user = User(user_entry[0], user_entry[1])
-            if (user.password == password):
-                return user
-    return None
+app.config['UPLOAD_FOLDER'] = './uploads'
+global_stuff = {}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form['api'])
-        if 'file' not in request.files:
-            flash('No File')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No File')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('index', filename=filename))
+    if global_stuff.has_key('current_user'):
+        if request.method == 'POST':
+            print(request.form['api'])
+            sys.stderr.write(request.form['api'])
+            if 'file' not in request.files:
+                return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                return redirect(request.url)
+            if file:
+                filename = secure_filename(file.filename)
+                if request.form['api'] == '0':
+                    sys.stderr.write("google drive")
+                    #ENCRYPTION HERE
+                    google_drive_wrapper = google_drive_functions.GoogleDriveWrapper()
+                    google_drive_wrapper.upload_file(filename)
+                if request.form['api'] == '1':
+                    sys.stderr.write("Dropbox")
+                    drop_box_wrapper = dropbox_functions.DropBoxWrapper()
+                    drop_box_wrapper.upload_file(filename)
+                if request.form['api'] =='2':
+                    sys.stderr.write("Box")
+                    #ENCRYPTION HERE
+                    box_wrapper = box_functions.BoxWrapper()
+                    box_wrapper.upload_file(filename)
+                if request.form['api'] == '3':
+                    sys.stderr.write("Local")
+                    encryption_wrapper = encrypt.EncryptWrapper()
+                    key = encryption_wrapper.generateKey(request.form['password'])
+                    # encryption_wrapper.encryptFile(filename, key)
+                    # encryption_wrapper.saveKey(key)
+                    # encryption_wrapper.encryptKey(filename + '.key')
 
-    return '''
-    <!doctype html>
-    <title>Upload New File</title>
-    <h1>Upload New File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p>
-        <select name=api>
-          <option value="0">Google Drive</option>
-          <option value="1">DropBox</option>
-          <option value="2">Box</option>
-          <option value="3">Local</option>
-        </select>
-        <input type=file name=file>
-        <input type=submit value=Upload>
-      </p>
-    </form>
-    '''
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-@app.route("/protected/", methods=['GET'])
-@login_required
-def protected():
-    return Response(response="Protected", status=200)
+                return redirect(url_for('index', filename=filename))
+
+        return '''
+        <!doctype html>
+        <title>Upload New File</title>
+        <h1>Upload New File</h1>
+        <form action="" method=post enctype=multipart/form-data>
+        <p>
+            <select name=api>
+            <option value="0">Google Drive</option>
+            <option value="1">DropBox</option>
+            <option value="2">Box</option>
+            <option value="3">Local</option>
+            </select>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+        </p>
+        <br>
+        <form action=/logout method=get>
+          <input type=submit value=logout>
+        </form>
+        '''
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if not global_stuff.has_key('current_user'):
+        if request.method == 'POST':
+            global_stuff['current_user'] = CredentialsWrapper(request.form['username'],
+                                                            request.form['password'])
+            return redirect(url_for('index'))
+        return '''
+        <!doctype html>
+        <title>Login</title>
+        <h1>Login</h1>
+        <form action="" method=post >
+        <p>
+            <input type=text name=username>
+            <input type=password name=password>
+            <input type=submit value=Upload>
+        </p>
+        </form>
+        '''
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if global_stuff.has_key('current_user'):
+        global_stuff.pop('current_user', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = "ITSASECRET"
