c592777e90abc856eea04ce422100eb15c32d09e
Nariana <nsands@smu.edu>
2016-12-04 18:29:58 -0600

    upload works

--- cse7339/gui/urls.py
+++ cse7339/gui/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'gui'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-##    url(r'^$', views.upload_file, name='upload'),
+    url(r'^upload/$', views.upload_file, name='upload'),
 ##    url(r'^$', views.handle_uploaded_file, name='upload_handle')
 ]
--- cse7339/gui/views.py
+++ cse7339/gui/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.template import loader
+from django.http import HttpResponseRedirect
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        file_name = fs.save(myfile.name, myfile)
+        context = {'file_name': file_name}
+        return render(request, 'upload.html', context)
+    return render(request, 'upload.html')
